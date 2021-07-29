c0e2ab139d5611c499c16682e8c9430c001fb6c9
Jamie <jamiejarrett2000@gmail.com>
2021-07-29 14:21:46 +0100

    Added Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Meme } from "./components/Meme";
 
 function App() {
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes").then((x) =>
+      x.json().then((response) => setTemplates(response.data.memes))
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <a className="navbar-brand" href="/">
+          <b>7 Days, 7 Projects - Meme Generator (Day 3/7)</b>
         </a>
-      </header>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="https://jj1.dev/projects">
+                More Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="memes" style={{ textAlign: "center" }}>
+        {template && <Meme template={template} />}
+        {!template &&
+          templates.map((template) => {
+            return (
+              <Meme
+                template={template}
+                onClick={() => {
+                  setTemplate(template);
+                }}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 }
